9b08f938991c101ac20a0dec97c7d750426419bc
tanve13 <171332553+tanve13@users.noreply.github.com>
2025-08-10 19:57:31 +0530

    Create PatternGeneration.java

---
+++ PatternGeneration.java
@@ -0,0 +1,14 @@
+ class PatternGeneration{
+  public static void main(String[]args){
+	    for(int i =0;i<5;i++){
+		    for (int j=0;j<5-i-1;j++) {
+                   System.out.print(" ");
+                }
+		    for(int k=0;k<2*i+1;k++){
+		       System.out.print("*");
+		    }
+		      System.out.println();
+
+	    }
+    }
+}
